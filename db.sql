5a07adbedfa62f1c102beb6e0dc7d58adcfcc657
Janani-bavi <147634484+Janani-bavi@users.noreply.github.com>
2024-11-24 21:06:25 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,14 +1,9 @@
-create database restaurent;
-use restaurent;
-CREATE TABLE customers (
-    cust_id VARCHAR(20) PRIMARY KEY,
-    cust_name VARCHAR(100) NOT NULL,   
-    food TEXT,                 
-    quantity INT NOT NULL            
-);
-INSERT INTO customers (cust_id, cust_name, food , quantity)
-VALUES 
-('R001', 'Janani', 'Biryani', 5),
-('R002', 'Mahitha', 'Dosa', 6),
-('R003', 'Abinaya', 'Noodles', 3);
-select * from customers;
+create database restaurant;
+use restaurant;
+CREATE TABLE menu_items(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL,   
+    price DECIMAL(10,2)NOT NULL,                 
+    description TEXT            
+)
+select * from menu_items;
